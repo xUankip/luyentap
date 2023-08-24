bfbd486a696d957208f3cf0a90c81958c724234c
xUankip <zzdark.knightzz97@gmail.com>
2023-08-24 11:13:15 +0700

    btvn

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -2,6 +2,9 @@
 #include <math.h>
     int main(){
         int i, a, b, c, d;
-        scanf("%d %d %d %d %d", &i, &a, &b, &c,&d);
-        printf("r1= %d\n", ++i % 7 );
+        scanf("%d%d%d%d%d", &i, &a, &b, &c, &d);
+        printf("r1= ++i %% 7= %d\n", ++i % 7 );
+        printf("r2= 5*(c-3+d)= %d\n", 5*(c-3+d));
+        printf("r3= a*(b+c/d)-22= %d\n", a*(b+c/d)-22);
+	return 0;
     }
